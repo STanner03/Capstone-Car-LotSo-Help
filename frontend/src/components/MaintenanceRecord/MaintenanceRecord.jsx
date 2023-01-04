7ca5f73efec5e7e810f4d4201e25130cb820abad
Shane <shanetanner03@gmail.com>
2023-01-04 14:25:11 -0600

    Making some minor CSS changes to make the text more clear and not so hard to read. + Making edits to the maintenance records to look less bulky.

--- frontend/src/components/MaintenanceRecord/MaintenanceRecord.jsx
+++ frontend/src/components/MaintenanceRecord/MaintenanceRecord.jsx
@@ -48,63 +48,69 @@ const MaintenanceRecord = ({
   };
   return (
     <div className="mr-style">
-      <button className="mr-but" onClick={handleSelect}>
-        <table>
-          <thead>
+      {show ? (
+        <div>
+          <button onClick={handleSelect}>See Details</button>
+          <table>
             <td>
-              Maintenance Record #{i}, {maintenance.date}
+              <tr>
+                <td>
+                  <p>#{i}</p>
+                </td>
+                <td>
+                  <p>Date: {maintenance.date}</p>
+                </td>
+                <td>
+                  <p>Shop: {maintenance.shop_name}</p>
+                </td>
+                <td>
+                  <p>Odometer: {maintenance.odometer} miles</p>
+                </td>
+                <td>
+                  <p>Total: ${maintenance.total_cost}</p>
+                </td>
+              </tr>
             </td>
-          </thead>
-          <td>
-            <tr>
-              <td>
-                <p>Shop: {maintenance.shop_name}</p>
-              </td>
-              <td>
-                <p>Odometer: {maintenance.odometer} miles</p>
-              </td>
-              <td>
-                <p>Total: ${maintenance.total_cost}</p>
-              </td>
-            </tr>
-          </td>
-        </table>
-      </button>
-      {show && (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Description</th>
-              <th>Miles to next service</th>
-              <th>Previous Odometer</th>
-              <th>Odometer</th>
-              <th>Total Price</th>
-              <th>Shop Name</th>
-              <th>Notes</th>
-              <th>
-                <button onClick={handleEditMaintenance}>
-                  Edit Maintenance
-                </button>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{maintenance.date}</td>
-              <td>{maintenance.description}</td>
-              <td>{maintenance.service_interval_miles}</td>
-              <td>{maintenance.prev_odometer}</td>
-              <td>{maintenance.odometer}</td>
-              <td>{maintenance.total_cost}</td>
-              <td>{maintenance.shop_name}</td>
-              <td>{maintenance.notes}</td>
-              <td>
-                <button onClick={handleDelete}>Delete Maintenance</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+          </table>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleSelect}>Hide Details</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Miles to next service</th>
+                <th>Previous Odometer</th>
+                <th>Odometer</th>
+                <th>Total Price</th>
+                <th>Shop Name</th>
+                <th>Notes</th>
+                <th>
+                  <button onClick={handleEditMaintenance}>
+                    Edit Maintenance
+                  </button>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{maintenance.date}</td>
+                <td>{maintenance.description}</td>
+                <td>{maintenance.service_interval_miles}</td>
+                <td>{maintenance.prev_odometer}</td>
+                <td>{maintenance.odometer}</td>
+                <td>{maintenance.total_cost}</td>
+                <td>{maintenance.shop_name}</td>
+                <td>{maintenance.notes}</td>
+                <td>
+                  <button onClick={handleDelete}>Delete Maintenance</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
